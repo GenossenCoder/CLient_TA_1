87d5e3f773f8eb85918fd8517e3506ee32f04748
GenossenCoder <xperte9988@gmail.com>
2021-04-20 14:29:02 +0200

    YEEEE

--- src/Components/MakePost.js
+++ src/Components/MakePost.js
@@ -1,7 +1,6 @@
 import React,{ useState } from 'react'
 import styles from '.././styles/MakePost.module.css'
 import {useMutation,gql} from '@apollo/client'
-import "react-toastify/dist/ReactToastify.css";
 
 function MakePost() {
     const CREATE_POST_MUTATION = gql`
@@ -22,6 +21,8 @@ function MakePost() {
     const [Name, setName] = useState("");
     const [Titel, setTitel] = useState("");
     const [Text, setText] = useState("");
+    const [Error, setError] = useState("")
+    const [Success, setSuccess] = useState("")
     const [createPost] = useMutation(CREATE_POST_MUTATION,{
             variables:{
                 Titel: Titel,
@@ -31,19 +32,25 @@ function MakePost() {
     })
     const Validate = ()=>{
         if(Name==="" || Text==="" || Titel===""){
-            window.location.reload(false);
+            setError("You have to fill all fields")
         }
         else{
             createPost();
+            setSuccess("You will be redirected soon")
+            setTimeout(()=>{
+                window.location.reload(true);
+            },2000)
+            
         }
     }
     return (
+        
         <form className={styles.PostInput} onSubmit={(e) => {
             e.preventDefault();
-            Validate()
-            
+            Validate();
           }}>
-            <h1 className={styles.Header}>Design the future</h1>
+            {Error && <li className={styles.Error}>{Error}</li>}
+            {Success && <li className={styles.Success}> {Success}</li>}
             <li ><input className={styles.PostShort} type="text" placeholder="Name" name="name" onChange={(e)=>{setName(e.target.value)}}></input></li>
             <li ><input className={styles.PostShort} type="text" placeholder="Title" name="title" onChange={(e)=>{setTitel(e.target.value)}}></input></li>
             <li ><textarea className={styles.PostText} type="text" placeholder="Inhalt" name="text" onChange={(e)=>{setText(e.target.value)}}></textarea></li>
